d3c4dd489f12d7993805f3ab4914661b93024cc0
Marcos <marcos27.bonfim@gmail.com>
2023-01-12 17:42:26 -0300

    requisito 2, 3 e alerta sem moeda

---
+++ src/main.js
@@ -0,0 +1,53 @@
+
+const url = 'https://api.exchangerate.host/latest?base=${moeda}';
+
+const botaoPesquisar = document.getElementById('search-btn');
+const inputMoeda = document.getElementById('input-moeda');
+
+
+botaoPesquisar.addEventListener('click', () => {
+    if (inputMoeda.innerHTML.length === 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Você precisa inserir uma moeda !',
+        });
+    }
+});
+
+const capturaMoedas = (rates) => {
+
+    const divPai = document.getElementById('container');
+    const arrRates = Object.entries(rates);
+    console.log(arrRates);
+
+    arrRates.map((rate) => {
+
+        const moeda = document.createElement('div');
+
+        moeda.setAttribute('value', rate);
+
+        moeda.innerText = rate;
+
+        divPai.appendChild(moeda);
+
+    });
+};
+
+
+const resultado = fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const { rates } = data;
+        capturaMoedas(rates);
+    });
+
+console.log(resultado);
+
+
+
+
+
+
+
+
